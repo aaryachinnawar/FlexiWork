4f52324467c0819d70bca1f974b36cff72e08ec1
rohitLanjewar03 <rohitlanjewar03@gmail.com>
2025-02-07 10:08:43 +0530

    No code changes made

--- src/components/Scene3D.jsx
+++
@@ -1,9 +0,0 @@
-import Spline from '@splinetool/react-spline';
-
-export default function App() {
-  return (
-    <div className="relative w-full h-full"> {/* Add positioning */}
-    <Spline scene="https://prod.spline.design/TxfHGkfXoX5p3CnN/scene.splinecode" />
-    </div>
-  );
-}
